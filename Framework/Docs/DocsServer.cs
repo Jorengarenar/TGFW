2f53ba07a275e1ddcf98ca4877fa84230bc6e61e
Grzegorz Brzozowski <gbrzozowski@WHM0006052.local>
2021-12-22 21:56:28 +0100

    add docs for framework

---
+++ Framework/Docs/DocsServer.cs
@@ -0,0 +1,24 @@
+﻿using Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Docs
+{
+    internal class DocsServer : Server
+    {
+        public DocsServer(string serverName, List<string> prefixes) : base(serverName, prefixes) { }
+
+        public override string Routing(string request)
+        {
+            string responseString = "";
+            if (request.Contains("docs"))
+            {
+                responseString = System.IO.File.ReadAllText("./docs/index.html");
+            }
+            return responseString;
+        }
+    }
+}
+
+
---
+++ Framework/Docs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Framework;
+using System.Linq;
+
+namespace Docs
+{
+    internal class Program
+    {
+
+        private static readonly string[] prefixes = { "docs" };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Docs server started");
+            Server server = new DocsServer("swagger", prefixes.ToList());
+        }
+    }
+}
