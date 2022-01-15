b6bf7710a184228e1c30dadfbd6df9a786687827
Grzegorz Brzozowski <dolidius@gmail.com>
2022-01-15 14:26:25 +0100

    chess api refactor

---
+++ Framework/ChessAsp/ChessGame.cs
@@ -0,0 +1,20 @@
+﻿using Framework;
+
+namespace ChessAsp {
+    public class ChessGame : Game {
+
+        public ChessGame() {
+            this.Board = new Board(8, 8);
+            for (int i = 0; i < 8; ++i) {
+                for (int j = 0; j < 8; ++j) { 
+                    Tile temp = this.Board.GetTile(i, j);
+                    if ((i * 8 + j) % 2 == 0) {
+                        temp.TexturePath = "img/chess/lightTile.png";
+                    } else {
+                        temp.TexturePath = "img/chess/darkTile.png";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Framework/ChessAsp/Controllers/ChessController.cs
@@ -0,0 +1,33 @@
+﻿using Framework;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ChessAsp.Repository;
+
+namespace ChessAsp.Controllers
+{
+    [Route("chess")]
+    [ApiController]
+    public class ChessController : ControllerBase
+    {
+        static readonly ChessRepository repository = new ChessRepository();
+        [HttpGet]
+        public IEnumerable<Game> Get()
+        {
+            return repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Game Get(int id)
+        {
+            return repository.Get(id);
+        }
+
+        [HttpPost]
+        public Game Post()
+        {
+            var newGame = new ChessGame();
+            repository.Add(newGame);
+            return newGame;
+        }
+    }
+}
