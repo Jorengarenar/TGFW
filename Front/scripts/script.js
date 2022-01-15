8b24b461b5305a5171ba2dd2a69e6db9bbe49072
Jorengarenar <dev@joren.ga>
2022-01-15 16:26:29 +0100

    Reindent

--- Front/scripts/script.js
+++ Front/scripts/script.js
@@ -2,47 +2,48 @@ const gamesList = document.querySelector("#gamesList");
 
 const getGamesList = () => {
 
-    const games = [{
-            name: "Chess",
-            img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Chess_Board.svg",
-            playersAmount: 4
-        },
-        {
-            name: "Mensch ärgere Dich nicht",
-            img: "https://upload.wikimedia.org/wikipedia/commons/9/91/Menschenaergern.svg",
-            playersAmount: "2-4"
-        }
-    ]
+  const games = [
+    {
+      name: "Chess",
+      img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Chess_Board.svg",
+      playersAmount: 4
+    },
+    {
+      name: "Mensch ärgere Dich nicht",
+      img: "https://upload.wikimedia.org/wikipedia/commons/9/91/Menschenaergern.svg",
+      playersAmount: "2-4"
+    }
+  ]
 
-    return games;
+  return games;
 }
 
 const showGamesList = () => {
-    const games = getGamesList();
+  const games = getGamesList();
 
-    games.forEach(game => {
-        const newGameEl = document.createElement("li");
-        newGameEl.classList.add("gamesBar__game");
+  games.forEach(game => {
+    const newGameEl = document.createElement("li");
+    newGameEl.classList.add("gamesBar__game");
 
-        const img = document.createElement("img");
-        img.src = game.img;
+    const img = document.createElement("img");
+    img.src = game.img;
 
-        const titleContainer = document.createElement("div");
+    const titleContainer = document.createElement("div");
 
-        const gameTitle = document.createElement("b");
-        gameTitle.textContent = game.name;
-        gameTitle.classList.add("title");
+    const gameTitle = document.createElement("b");
+    gameTitle.textContent = game.name;
+    gameTitle.classList.add("title");
 
-        const playersAmount = document.createElement("i")
-        playersAmount.textContent = `${game.playersAmount} Players`;
+    const playersAmount = document.createElement("i")
+    playersAmount.textContent = `${game.playersAmount} Players`;
 
-        titleContainer.appendChild(gameTitle);
-        titleContainer.appendChild(playersAmount);
-        newGameEl.appendChild(img);
-        newGameEl.appendChild(titleContainer);
+    titleContainer.appendChild(gameTitle);
+    titleContainer.appendChild(playersAmount);
+    newGameEl.appendChild(img);
+    newGameEl.appendChild(titleContainer);
 
-        gamesList.appendChild(newGameEl);
-    });
+    gamesList.appendChild(newGameEl);
+  });
 
 }
 
