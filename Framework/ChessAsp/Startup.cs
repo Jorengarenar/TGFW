8581c35846d5888cb12d21a6ba9fd3d24f168a07
Grzegorz Brzozowski <dolidius@gmail.com>
2022-01-16 01:06:48 +0100

    allow cors

--- Framework/ChessAsp/Startup.cs
+++ Framework/ChessAsp/Startup.cs
@@ -32,19 +32,23 @@ namespace ChessAsp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Chess", Version = "v1" });
             });
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChessAsp v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChessAsp v1"));
 
             app.UseHttpsRedirection();
+
+            app.UseCors(options => options.AllowAnyOrigin());  
 
             app.UseRouting();
 
