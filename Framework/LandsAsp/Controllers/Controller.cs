b2e04f291cb06ee550382507629e4b69238353d7
DawidMoza <dawidmoza@gmail.com>
2022-03-18 05:55:06 +0100

    able to make moves!

--- Framework/LandsAsp/Controllers/Controller.cs
+++ Framework/LandsAsp/Controllers/Controller.cs
@@ -12,16 +12,36 @@ namespace LandsAsp.Controllers {
 
         private static readonly Repository repository = new Repository();
 
-        [HttpGet]
+        [HttpGet("get")]
         public LandsGame Get(string id) {
             return (LandsGame) repository.Get(id);
         }
 
-        [HttpPost]
-        public string Post(string firstName, string secondName) {
+        [HttpGet("waiting")]
+        public int WaitingFor(string id) {
+            LandsGame game = (LandsGame) repository.Get(id);
+            return game.turnsMediator.waitingFor;
+        }
+
+        [HttpPost("create")]
+        public string Create(string firstName, string secondName, int width, int height) {
             IUserInterface userInterface = new WebUserInterface();
             List<LandsPlayerData> players = new List<LandsPlayerData>() { new LandsPlayerData(firstName), new LandsPlayerData(secondName)};
-            return repository.Add(new LandsGame(2, 2, players, userInterface, TurnsMediator.Mediators.Web));
+            return repository.Add(new LandsGame(width, height, players, userInterface, TurnsMediator.Mediators.Web));
+        }
+
+        [HttpPost("tile")]
+        public string PlaceTile(string id, int player, int availableTileIndex, int tileX, int tileY) {
+            LandsGame game = (LandsGame) repository.Get(id);
+            game.turnsMediator.Notify(player, $"tile:{availableTileIndex};{tileX};{tileY}");
+            return "ok";
+        }
+
+        [HttpPost("meeple")]
+        public string PlaceMeeple(string id, int player, int pieceIndex, int tileX, int tileY) {
+            LandsGame game = (LandsGame) repository.Get(id);
+            game.turnsMediator.Notify(player, $"meeple:{pieceIndex};{tileX};{tileY}");
+            return "ok";
         }
     }
 }
