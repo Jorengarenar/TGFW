493580ba0576c06e8c3b57ca00107bee93b30b8f
Jorengarenar <dev@joren.ga>
2022-04-27 09:06:40 +0200

    Move back to own method

--- Front/Framework/script.js
+++ Front/Framework/script.js
@@ -11,66 +11,7 @@ class TGFW {
     this.CLICKED_TILE = null;
     this.CONTAINER = null;
 
-    window.onload = function() { // init DOM
-      this.CONTAINER = document.querySelector("#TGFW");
-
-      const newEl = (tag) => document.createElement(tag);
-
-      // nav {{{1
-      const nav = newEl("nav");
-
-      // gameIdDiv {{{2
-      const gameIdDiv = newEl("div");
-      gameIdDiv.id = "gameId";
-
-      // optionsDiv {{{2
-      const optionsDiv = newEl("div");
-      optionsDiv.id = "options";
-
-      // newGameBtn {{{3
-      const newGameBtn = newEl("button");
-      newGameBtn.id = "newGame";
-      newGameBtn.onclick = () => { this.newGame() };
-      newGameBtn.textContent = "Host new game";
-
-      // joinGameBtn {{{3
-      const joinGameBtn = newEl("button");
-      joinGameBtn.id = "joinGame";
-      joinGameBtn.onclick = () => { this.joinGame() };
-      joinGameBtn.textContent = "Join game";
-
-      // separator {{{3
-      const separator = newEl("span");
-      separator.classList.add("separator")
-
-      // resetGameBtn {{{3
-      const resetGameBtn = newEl("button");
-      resetGameBtn.id = "resetGame";
-      resetGameBtn.onclick = () => { this.resetGame() };
-      resetGameBtn.textContent = "Reset game";
-      // }}}3
-
-      optionsDiv.append(newGameBtn, joinGameBtn, resetGameBtn);
-      // }}}2
-
-      nav.append(gameIdDiv, optionsDiv);
-
-      // gameDiv {{{1
-      const gameDiv = newEl("div");
-      gameDiv.id = "game";
-
-      const boardDiv = newEl("div");
-      boardDiv.id = "board";
-
-      const piecesDiv = newEl("div");
-      piecesDiv.id = "pieces";
-
-      gameDiv.append(boardDiv, piecesDiv);
-      // }}}1
-
-      this.CONTAINER.append(nav, gameDiv);
-
-    }
+    window.onload = () => { tgfw.initDom(); }
   }
 
   updateGameId(id) {
@@ -107,6 +48,67 @@ class TGFW {
     if (this.updateGameId(prompt("Type game ID:"))) {
       this.drawBoard();
     }
+  }
+
+  initDom() {
+    this.CONTAINER = document.querySelector("#TGFW");
+
+    const newEl = (tag) => document.createElement(tag);
+
+    // nav {{{1
+    const nav = newEl("nav");
+
+    // gameIdDiv {{{2
+    const gameIdDiv = newEl("div");
+    gameIdDiv.id = "gameId";
+
+    // optionsDiv {{{2
+    const optionsDiv = newEl("div");
+    optionsDiv.id = "options";
+
+    // newGameBtn {{{3
+    const newGameBtn = newEl("button");
+    newGameBtn.id = "newGame";
+    newGameBtn.onclick = () => { this.newGame() };
+    newGameBtn.textContent = "Host new game";
+
+    // joinGameBtn {{{3
+    const joinGameBtn = newEl("button");
+    joinGameBtn.id = "joinGame";
+    joinGameBtn.onclick = () => { this.joinGame() };
+    joinGameBtn.textContent = "Join game";
+
+    // separator {{{3
+    const separator = newEl("span");
+    separator.classList.add("separator")
+
+    // resetGameBtn {{{3
+    const resetGameBtn = newEl("button");
+    resetGameBtn.id = "resetGame";
+    resetGameBtn.onclick = () => { this.resetGame() };
+    resetGameBtn.textContent = "Reset game";
+    // }}}3
+
+    optionsDiv.append(newGameBtn, joinGameBtn, resetGameBtn);
+    // }}}2
+
+    nav.append(gameIdDiv, optionsDiv);
+
+    // gameDiv {{{1
+    const gameDiv = newEl("div");
+    gameDiv.id = "game";
+
+    const boardDiv = newEl("div");
+    boardDiv.id = "board";
+
+    const piecesDiv = newEl("div");
+    piecesDiv.id = "pieces";
+
+    gameDiv.append(boardDiv, piecesDiv);
+    // }}}1
+
+    this.CONTAINER.append(nav, gameDiv);
+
   }
 }
 
